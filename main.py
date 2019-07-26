c1243580c64f78d388afccd569847c643777e2a3
kitsunetohu <bofenghui@gmail.com>
2019-07-26 20:14:56 +0900

    rebuild frame

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
-import  numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+import numpy as np
+from sklearn.linear_model import LogisticRegression
 
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn import datasets
 
-obj = KNeighborsClassifier()
+#df=pd.read_csv("bank.csv",usecols=range(0,1))
+#选取特定范围的列
 
-iris = datasets.load_iris()
-x = iris.data
-y = iris.target
-
-print(x[:4, :])
-print(y)
-obj.fit(x, y)
-y_pred = obj.predict(x[:4, :])
-
-print(y_pred)
+df=pd.read_csv("bank.csv")
+df=df['age;"job";"marital";"education";"default";"balance";"housing";"loan";"contact";"day";"month";"duration";"campaign";"pdays";"previous";"poutcome";"y"'].str.split(';',expand=True)
+df.rename(columns={0: 'age', 1: 'job',2: 'marital',3: 'education',4: 'default',5: 'balance',6: 'housing',7: 'loan',8: 'contact',9: 'day',10: 'month',11: 'duration',12: 'campaign',13: 'pdays',14: 'previous',15: 'poutcome',16:"y"}, inplace=True)
+print(df)
